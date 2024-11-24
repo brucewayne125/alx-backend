79ec6e4e2d001a946197ff630e27469675d24db4
brucewayne125 <snowdbat@gmail.com>
2024-11-24 14:48:42 -0800

    Task1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,14 +4,14 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
-class config:
+class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.config.from_object(config)
+app.config.from_object(Config)
 
 babel = Babel(app)
 
