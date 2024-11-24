ade7bfe72408379b78dd8b5bebf2c1849efd0dc8
brucewayne125 <snowdbat@gmail.com>
2024-11-24 04:07:43 -0800

    Task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-#Flask - the core class from the Flask framework
-#that helps set up a web application.
-#render_template - function used to render HTML templates
-#@app.route('/') - specifies the URL route that will trigger the home function
-#index - function that runs when the root route is accessed
+# Flask - the core class from the Flask framework
+# that helps set up a web application.
+# render_template - function used to render HTML templates
+# @app.route('/') - specifies the URL route that will trigger the home function
+# index - function that runs when the root route is accessed
 
 from flask import Flask, render_template
 
