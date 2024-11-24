513b5215461d97768252ccd92007b811565c7ec3
brucewayne125 <snowdbat@gmail.com>
2024-11-24 14:54:17 -0800

    Task1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-
+# index - function that returns endered HTML from the `1-index.html` template
+# Config -  Configuration class for the Flask app
 
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 class Config:
     LANGUAGES = ["en", "fr"]
@@ -14,6 +16,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
 
 @app.route('/')
 def index():
