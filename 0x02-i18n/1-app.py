8bb9f36dda36b65dfe0a326deea9dfdd1be7a5b9
brucewayne125 <snowdbat@gmail.com>
2024-11-24 11:35:59 -0800

    Task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+
+from flask import Flask, render_template
+from flask_babelex import Babel
+
+class config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
